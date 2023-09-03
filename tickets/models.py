d8d61f47c7d8e99ebf604d80475b478da33ff6f6
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-03 23:31:00 +0300

    create models for tikcets process

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    hall = models.CharField(max_length=20)
+    movie = models.CharField(max_length=20)
+    data = models.DateField()
+    def __str__(self):
+        return self.movie
+class Guest(models.Model):
+    name  = models.CharField(max_length=20)
+    mobile =models.CharField(max_length = 15)
+    def __str__(self):
+        return self.name
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest,related_name='reservation_guest',on_delete= models.CASCADE)
+    movie = models.ForeignKey(Movie,related_name = 'reservation_movie',on_delete= models.CASCADE)
+    def __str__(self):
+        return f"{self.guest}--->{self.movie}"
