df255a4d30190eede899bd8ddd3948ee16cf9d2b
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-04 13:56:05 +0300

    Create view  Without rest and no model query

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-
-# Create your views here.
+ # Create your views here.
+# Without rest and no model query :
+def no_rest_no_model(request):
+    guests = [
+        {
+            'id':1,
+            'name':'mustafa',
+        }
+        ,
+        {
+            'id':2,
+            'name':'ali',
+        }
+    ]
+    return JsonResponse(guests,safe=False)
