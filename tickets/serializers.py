3348b67d0e97fb8ba67f7962d048cc018f1bf9c8
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-04 03:45:24 +0300

    create a serialzers for all models

---
+++ tickets/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import *
+
+class MovieSerialzers(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields ='__all__'
+class ReservationSerialzers(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields ='__all__'
+class GuestSerialzers(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields =['pk','reservation_guest','name','mobile']
+        
