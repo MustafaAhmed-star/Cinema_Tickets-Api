ad110f895aa698297e39322dc2c667bcf274fa86
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-04 19:46:44 +0300

    list_create_api created and add a pattern

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from tickets  import views 
 urlpatterns = [
+     
     path("admin/", admin.site.urls),
+    #1
     path('no-rest-no-model',views.no_rest_no_model),
-    path('no-rest-from-model',views.no_rest_from_model)
+    #2
+    path('no-rest-from-model',views.no_rest_from_model),
+    #3
+    path('rest/list-create', views.list_create_api)
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,8 +1,11 @@
-from django.http import JsonResponse
+from django.http.response import JsonResponse  
 from django.shortcuts import render
 from .models import Movie,Guest,Reservation
-
- # Create your views here.
+from rest_framework.decorators import api_view
+from .serializers import *
+from rest_framework import status
+from rest_framework.response import Response
+'''
 # Without rest and no model query :
 def no_rest_no_model(request):
 
@@ -25,4 +28,22 @@ def no_rest_from_model(request):
     response={
         'guests':list(data.values('name','mobile'))
     }
-    return JsonResponse(response,safe=False)
+    return JsonResponse(response )
+'''
+#Function based view 
+#GET POST
+@api_view('GET',"POST")
+def list_create_api(request):
+    #GET
+    if request.method == 'GET':
+        gusets= Guest.objects.all()
+        serializer = GuestSerialzers(gusets,many = True)
+        return Response(serializer.data)
+    #post
+    elif request.method == 'POST':
+        serializer  = GuestSerialzers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status= status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+    
